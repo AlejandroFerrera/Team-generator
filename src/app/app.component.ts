fc1f9b8a4ab206d021a819f9a32ee95873d2820d
Alejandro_Ferrera <aferrerah1421@gmail.com>
2022-08-30 03:30:55 -0400

    added style and functionalities to add member and list them

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'team-generator';
+  newMemberName = "";
+  members: string[] = [];
+
+  addMember() {
+    this.members.push(this.newMemberName);
+    this.newMemberName = "";
+    console.log(this.members)
+  }
+
+  onInput(member: string) {
+    this.newMemberName = member;
+    console.log(this.newMemberName);
+  }
 }
